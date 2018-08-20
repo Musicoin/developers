cfcdae118ec9d30c7a5a289dd6e44e291f39334f
Adrián Rueda <enmaska@gmail.com>
2018-08-20 02:43:57 +0200

    apidocs changes

--- src/ApiDocsPage/ApiDocsPage.jsx
+++ src/ApiDocsPage/ApiDocsPage.jsx
@@ -17,16 +17,57 @@ class ApiDocsPage extends React.Component {
         <Header />
         <Container>
 
-          <h1 className='super-big mt80'>Web API Documentation</h1>
-          <p className='lead'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-          <div className='row mt80 mb40'>
-            <img src="/img/webapi/webapi_intro.png" alt=""/>
-          </div>
+          <h1 className='titlePage mt80'>Web API Documentation</h1>
+          <p>
+            Note: By using Musicoin developer tools, you accept the Musicoin Developer Terms of Service.
+          </p>
+          <p>
+          Based on simple REST principles, the Musicoin Web API endpoints return JSON metadata about music artists, albums, and tracks, directly from the Musicoin Data Catalogue.
+          </p>
+          <img src='/img/webapi/webapi_intro.png' alt='' className='responsive' />
+          <p>
+          Web API also provides access to user related data, like playlists and music that the user saves in the Your Music library. Such access is enabled through selective authorization, by the user.
+          </p>
+          <p>
+          The base address of Web API is <a href='https://api.musicoin.com'>https://api.musicoin.com</a>. The API provides a set of endpoints, each with its own unique path. To access private data through the Web API, such as user profiles and playlists, an application must get the user’s permission to access the data. Authorization is via the Musicoin Accounts service.
+          </p>
+          <h2>Requests</h2>
+          <p>
+          The Musicoin Web API is based on REST principles. Data resources are accessed via standard HTTPS requests in UTF-8 format to an API endpoint. Where possible, Web API uses appropriate HTTP verbs for each action:
+          </p>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Method</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>GET</td>
+                <td>Retrieves resources</td>
+              </tr>
+              <tr>
+                <td>POST</td>
+                <td>Creates resources</td>
+              </tr>
+              <tr>
+                <td>PUT</td>
+                <td>Changes and/or replaces resources or collections</td>
+              </tr>
+              <tr>
+                <td>DELETE</td>
+                <td>Deletes resources</td>
+              </tr>
+            </tbody>
+          </table>
+
           <div>
-          <Highlight innerHTML={true}>{'<p>Hello world</p>'}</Highlight>
-          <Highlight language="javascript">
-            {`function foo() { return 'bar' }`}
-          </Highlight>
+            <h2>Hello World</h2>
+            <Highlight language='javascript'>
+              {`function foo() { return 'bar' }`}
+            </Highlight>
           </div>
         </Container>
       </div>
